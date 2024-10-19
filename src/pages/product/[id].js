5ff6eb88950032bd493729798d1a3ae4898ec8f5
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-10-19 22:08:28 +0500

    updated

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -287,8 +287,14 @@ export default function Product() {
         )}
 
         {isModalOpen && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-            <div className="relative max-w-3xl mx-auto p-4">
+          <div
+            onClick={closeModal}
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
+          >
+            <div
+              className="relative max-w-3xl mx-auto p-4"
+              onClick={(e) => e.stopPropagation()}
+            >
               <button
                 className="absolute top-[-25px] right-[-25px] p-2 text-white"
                 onClick={closeModal}
